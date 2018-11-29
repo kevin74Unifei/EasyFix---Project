add26e0d2a0da99c8c65208cfdad9aad8bf6df28
LucasLamounierD <lucaslamounier13@gmail.com>
2018-11-29 00:12:00 -0200

    Inicio do album

--- resources/views/crud-album/AlbumView.blade.php
+++ resources/views/crud-album/AlbumView.blade.php
@@ -1,7 +1,38 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: windows
- * Date: 28/11/2018
- * Time: 23:37
- */
+
+@extends('../templates/template_base')
+
+@section('tools-icon')
+@endsection
+@section('menu')
+@endsection
+
+@section('Base')
+    <style>
+        .pagina{position: absolute;
+            top:80px;
+            left:350px;
+            width:1050en;
+            background-color: whitesmoke;
+            padding: 4%;
+            padding-top:20px;
+            padding-bottom:100px;
+        }
+
+        .img_view{
+            height: 500px;
+            width: 500px;
+        }
+
+    </style>
+
+    <div class="pagina">
+
+<div class="row">
+    <div class="col-md-6">
+        <img src="" class="img_view">
+    </div>
+
+</div>
+
+    </div>
+@endsection
